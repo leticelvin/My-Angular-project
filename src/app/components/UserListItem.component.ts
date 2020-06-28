c37c87f42443d41025aa4ad1f8c54dc87ee7fbde
leticelvin <minecraft92@hotmail.com>
2020-06-29 01:05:16 +0200

    Finished!

--- src/app/components/UserListItem.component.ts
+++ src/app/components/UserListItem.component.ts
@@ -2,13 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: "app-root",
-    template: `<li class="userlistitem" [style.color]="TextColor">{{name}}</li>`,
+    template: `<li class="userlistitem" [style.color]="TextColor">{{user}}</li>`,
     styleUrls: ['./test.component.css']
 })
 
 export class UserListItem {
-    public name: string = "Tanri en Buyuktur"
 
-    @Input('parentData') public user;
+    @Input('parentData') public user: String;
     @Input('TextColor') public textcolor: String;
 }
